a80f25ded3ec6cbb91f330c255c75be33d1c8913
liztrg <92203804+liztrg@users.noreply.github.com>
2023-09-24 09:36:18 +0200

    Update dataloader.py

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,19 +1,18 @@
 import pandas as pd
 import numpy as np
-from sklearn.compose import make_column_transformer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
 def dataloader():
-    returns = pd.read_csv("data/size_inv_data.csv")
-    market_data = pd.read_csv("data/market_data.csv")
+    returns = pd.read_csv("data//size_inv_data.csv")
+    market_data = pd.read_csv("data//market_data.csv")
 
     start_date = 196404  # April 1964
     end_date = 202106  # June 2021
 
     returns = returns[(returns['Month'] >= start_date) & (returns['Month'] <= end_date)]
     market_data = market_data[(market_data['Month'] >= start_date) & (market_data['Month'] <= end_date)]
-    print(returns)
+    #print(returns)
     #print(market_data)
     #print(market_data['RF'])
 
@@ -24,7 +23,14 @@ def dataloader():
         market_data.set_index('Month', inplace=True)
 
     returns = returns.sub(market_data['RF'], axis=0)
-    print(market_data['RF'])
-    print(returns)
+    #print(market_data['RF'])
+    #print(returns)
 
-    return returns, market_data['Mkt-RF']
+    return returns, market_data['Mkt-RF'],market_data['RF']
+
+def save_results_csv(file_path, results):
+    # Write the DataFrame to a CSV file
+    results.to_csv(file_path, index=False)  # Set index=False to exclude the index column
+
+    # Optionally, print a message to confirm the save
+    print(f'Results saved to {file_path}')
